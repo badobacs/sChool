b4ea20bd3377e5c2d3236ce2e357e530745afc63
János Fleck <janos.fleck@gmail.com>
2023-07-26 23:14:21 +0200

    BF1

---
+++ Methods Lessons/cube1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static int MyMethod(int x, int y)
+        {
+            Console.WriteLine("Ird be az első számot");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ird be a második számot ");
+            y = Convert.ToInt32(Console.ReadLine());
+            if (x > y)
+            {
+                return x * y;
+            }
+            else
+            {
+                return x + y;
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(MyMethod(2 ,3));
+        }
+    }
+}
