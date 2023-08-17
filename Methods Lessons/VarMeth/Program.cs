86a400a32c79d1df0f3f215deb5bdc308db94967
János Fleck <janos.fleck@gmail.com>
2023-08-17 22:51:31 +0200

    variation

---
+++ Methods Lessons/VarMeth/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace VarMeth
+{
+    class Program
+    {
+        static void Cube()
+        {
+            //Console.WriteLine("Enter the size of the margin:");
+            //int s = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the length of the side of the cube:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //int k = 5;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+
+                    Console.Write("* ");
+                }
+                Console.WriteLine("*");
+
+            }
+        }
+        static void Triangle()
+        {
+            //Console.WriteLine("Enter the size of the margin:");
+            //int s = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the length of the side of the triangle:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //int k = 5;
+            for (int i = 1; i<=n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+        static void DecreasingTriangle()
+        {
+            //Console.WriteLine("Enter the size of the margin:");
+            //int s = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the length of the side of the DecreasingTriangle:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //int k = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= n; j++)
+                {
+
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+        static void RsTriangle()
+        {
+            
+            Console.WriteLine("Enter the length of the side of the Sandglass:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            //int k = 5;
+
+            for (int i = 1; i <=n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = i; j <= n; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Cube();
+            Triangle();
+            DecreasingTriangle();
+            RsTriangle();
+        }
+    }
+}
