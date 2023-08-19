515a50d4ca05648d73ad958c521566576775b492
János Fleck <janos.fleck@gmail.com>
2023-08-19 12:47:12 +0200

    day2

---
+++ Methods Lessons/sqare/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sqare
+{
+    class Program
+    {
+        static void Square()
+        {
+            int n = 5;
+                for (int i = 1; i <=n; i++)
+            {
+                for (int j = 0; j <=n; j++)
+                {
+                    if (j==1 || j ==n)
+                    {
+
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Square();
+        }
+    }
+}
